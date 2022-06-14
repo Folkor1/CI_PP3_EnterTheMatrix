add195851f71a38783489db3055068c4179576cb
Folkor1 <igorvasiljevs@gmail.com>
2022-06-14 20:22:13 +0000

    Add colors class and program logo

--- run.py
+++ run.py
@@ -12,3 +12,26 @@ CREDS = Credentials.from_service_account_file('creds.json')
 SCOPED_CREDS = CREDS.with_scopes(SCOPE)
 GSPREAD_CLIENT = gspread.authorize(SCOPED_CREDS)
 SHEET = GSPREAD_CLIENT.open('Enter_the_Matrix')
+
+class colors:
+    GREEN = '\033[92m'
+    WHITE = '\033[97m'
+
+def welcome():
+    """
+    Welcome message.
+    """
+    print("\n\n")
+    print("=" * 70)
+    print("Hello and welcome to this very useful matrix determinant finder tool!")
+    print("=" * 70)
+    print("\n                               Enter the")
+    print(colors.GREEN + "              __  __           _            _        ")
+    print(colors.GREEN + "             |  \/  |   __ _  | |_   _ __  (_) __  __")
+    print(colors.GREEN + "             | |\/| |  / _` | | __| | '__| | | \ \/ /")
+    print(colors.GREEN + "             | |  | | | (_| | | |_  | |    | |  >  < ")
+    print(colors.GREEN + "             |_|  |_|  \__,_|  \__| |_|    |_| /_/\_\ ")
+    print(colors.WHITE + " ")
+    print("=" * 70)
+    print("")
+    print("=" * 70)
