d247cf3029f6e6f02720595e5209338517b2538e
Folkor1 <igorvasiljevs@gmail.com>
2022-06-14 20:43:10 +0000

    Add matrix 3x3 child class

--- matrix.py
+++ matrix.py
@@ -21,3 +21,27 @@ class Matrix_2x2(Matrix):
     
     def determinant_2x2(self):
         return f'{super().calc_2x2()}'
+
+class Matrix_3x3(Matrix):
+    """
+    3x3 matrix class,
+    calculates 3x3 matrix determinant.
+    """
+    def __init__(self, num1, num2, num3, num4, num5, num6, num7, 
+    num8, num9):
+        Matrix.__init__(self, num1, num2, num3, num4)
+        self.num5 = num5
+        self.num6 = num6
+        self.num7 = num7
+        self.num8 = num8
+        self.num9 = num9
+    
+    def determinant_3x3(self):
+        a = self.num1 * self.num5 * self.num9
+        b = self.num2 * self.num6 * self.num7
+        c = self.num3 * self.num4 * self.num8
+        d = self.num3 * self.num5 * self.num7
+        e = self.num2 * self.num4 * self.num9
+        f = self.num1 * self.num6 * self.num8
+        
+        return a + b + c - d - e - f
