2c1181fdc5b2ccba12dffeb6172b6a63e0321314
Folkor1 <igorvasiljevs@gmail.com>
2022-06-15 12:54:07 +0000

    Add unit test for 3x3 matrix

--- tests.py
+++ tests.py
@@ -10,5 +10,12 @@ class TestMatrix(unittest.TestCase):
         msg_2x2 = "Matrix 2x2 determinant caclulation is not correct!"
         self.assertEqual(calculation, determ_2x2, msg_2x2)
 
+    def test_matrix_3x3_input(self):
+        numbers = matrix.Matrix_3x3(2,6,7,2,0,8,1,2,5)
+        calculation = numbers.determinant_3x3()
+        determ_3x3 = -16
+        msg_3x3 = "Matrix 3x3 determinant caclulation is not correct!"
+        self.assertEqual(calculation, determ_3x3, msg_3x3)
+
 if __name__ == '__main__':
     unittest.main()
