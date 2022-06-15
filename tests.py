6efd3eaad7222a2ff909cf8f9dcace6f6c8bcfba
Folkor1 <igorvasiljevs@gmail.com>
2022-06-15 12:57:56 +0000

    Add spaces between numbers

--- tests.py
+++ tests.py
@@ -1,24 +1,25 @@
 import unittest
 import matrix
 
+
 class TestMatrix(unittest.TestCase):
 
     def test_matrix_2x2_input(self):
-        numbers = matrix.Matrix_2x2(1,2,3,4)
+        numbers = matrix.Matrix_2x2(1, 2, 3, 4)
         calculation = numbers.determinant_2x2()
         determ_2x2 = '-2'
         msg_2x2 = "Matrix 2x2 determinant caclulation is not correct!"
         self.assertEqual(calculation, determ_2x2, msg_2x2)
 
     def test_matrix_3x3_input(self):
-        numbers = matrix.Matrix_3x3(2,6,7,2,0,8,1,2,5)
+        numbers = matrix.Matrix_3x3(2, 6, 7, 2, 0, 8, 1, 2, 5)
         calculation = numbers.determinant_3x3()
         determ_3x3 = -16
         msg_3x3 = "Matrix 3x3 determinant caclulation is not correct!"
         self.assertEqual(calculation, determ_3x3, msg_3x3)
 
     def test_matrix_4x4_input(self):
-        array = (7,5,0,1,2,8,5,3,8,0,2,3,6,5,8,2)
+        array = (7, 5, 0, 1, 2, 8, 5, 3, 8, 0, 2, 3, 6, 5, 8, 2)
         numbers = matrix.Matrix_4x4(*array)
         calculation = numbers.determinant_4x4()
         determ_4x4 = -1199
